75bc4496ba313dd3183e272e77c0621e32f64557
Rajit kumar <krajit909@gmail.com>
2024-10-23 11:15:24 +0530

    bug fix

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const DoctorCard = ({ image, name, specialty, bio }) => {
   return (
     <div className="bg-white border-none shadow-md rounded-lg overflow-hidden transform hover:scale-[101%] transition-all duration-300 " >
-      <img src={image} alt={name} className="w-full min-h-[350px] max-h-[350px]" />
+      <img src={image} alt={name} className="w-full min-h-[320px] md:min-h-[380px] max-h-[380px]" />
       <div className="p-4 dark:bg-slate-800">
         <h3 className="text-xl font-bold text-blue-600 mb-2">{name}</h3>
         <p className="text-gray-700 dark:text-white  font-medium">{specialty}</p>
