27695da9ad3f3ad7c3ae021b7f51180a58c7e183
Rajit kumar <krajit909@gmail.com>
2024-10-23 13:32:13 +0530

    bug fix

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -34,7 +34,7 @@ const Events = () => {
   return (
     <section id="events" className="py-12 bg-gray-50 dark:bg-slate-800 ">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center text-gray-800 dark:text-white mb-8">
+        <h2 className=" text-2xl md:text-3xl font-bold text-center text-gray-800 dark:text-white mb-8">
           Events & Workshops
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
